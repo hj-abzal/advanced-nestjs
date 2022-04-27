98bffe14f31c2e932380dff15748eefce934fa16
Abzal <abzal008suan@gmail.com>
2022-04-28 02:07:14 +0600

    add small changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,11 +25,11 @@ import * as path from "path";
     }),
     SequelizeModule.forRoot({
       dialect: "postgres",
-      host: process.env.POSTGRES_HOST || "ec2-52-18-116-67.eu-west-1.compute.amazonaws.com",
-      port: Number(process.env.POSTGRES_PORT) || 5432,
-      username: process.env.POSTGRES_USER || "cfnawxcgfojinq",
-      password: process.env.POSTGRES_PASSWORD || "bdcbd68eccd924a6e03003b4766eb32df2a99dcca09f0fb52c8ee318e095d5d2",
-      database: process.env.POSTGRES_DB || "d4lok4st4c4rp4",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [User, Role, UserRoles, Posts],
       autoLoadModels: true
     }),
